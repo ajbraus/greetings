289df67468fad876a87497c8ea65ad11212b0a0e
Adam Braus <ajbraus@gmail.com>
2023-08-30 10:13:23 -0700

    initial commit

---
+++ greetings.py
@@ -0,0 +1,17 @@
+print("hello")
+name = "Adam Braus"
+# the string method + "concatination" 
+# print('Greetings, ' + name)
+
+# the number method + "addition"
+apples = 4
+oranges = 10
+
+print(apples + oranges)
+
+# types 
+str = "stuff" # a string
+num = 3243
+
+# using 'f-string' pattern to interpolate variables into a string.
+print(f"Greetings, {name}. There are {apples} apples, and {oranges} oranges.")
